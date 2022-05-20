aed1179526e2544dcc314fe55da8d7fdc2f4f9b1
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-05-20 10:41:55 +0530

    din

---
+++ asign-1/src/compo/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+    return (<div>
+<h1>Mobile Operating System</h1>
+<ul>
+    <li>Android</li>
+    <li>Blackberry</li>
+    <li>iPhone</li>
+    <li>Window Phone</li>
+</ul>
+<h1>Mobile Manufacturers</h1>
+<ul>
+    <li>Samsung</li>
+    <li>Htc</li>
+    <li>Micromax</li>
+    <li>Apple</li>
+</ul>
+    </div>)
+}
+export default Card;
