fc006f30acd4d7c7c88e21c17d961c368c96552c
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-05-22 18:20:32 +0530

    din

---
+++ Comp/src/component/Ctodo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ctodo = () => {
+  return (
+    <div>C-todo</div>
+  )
+}
+
+export default Ctodo
---
+++ Comp/src/component/Todo.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const todo = () => {
+    const [todo, setTodo] = useState([]);
+    return(
+         <div>todo</div>
+         )
+}
+
+export default todo;
