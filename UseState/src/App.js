bc31e4bfba15451d7f4190518dbe9ea9e1f0316d
CYNO28 <44347226+CYNO28@users.noreply.github.com>
2022-05-25 13:58:59 +0530

    0

---
+++ UseState/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todo from './components/Todos';
+function App() {
+ 
+  return (
+    <div className="App">
+      Hello
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ UseState/src/components/Todos.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+const Todos = () => {
+    const[newTodo,setnewTodos]=useState("");
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
+  function putdata() {
+  fetch("http://localhost:8080/posts",
+    {method: 'POST',
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+        text: newTodo,
+        isCompleted: false
+    }
+        )}).then(res => res.json())
+        .then(data => {
+            setTodos([...todos, data]);
+            console.log(data);
+            setnewTodos("");
+            })
+  
+    }
+  return <div>
+      <div>
+        <input type="text" onChange={(e)=>{setnewTodos(e.target.value)}} />
+        <button onClick={putdata}>+add</button>
+          {todos.map((todo) => <div key={todo.id}>{todo.text}</div>)}
+      </div>
+      </div>;
+};
+
+export default Todos;
