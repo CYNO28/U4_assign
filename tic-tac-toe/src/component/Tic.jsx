27ca2286ffb90b313f3eeb97fa3facf19511d94b
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-05-27 00:24:35 +0530

    1

--- tic-tac-toe/src/component/Tic.jsx
+++ tic-tac-toe/src/component/Tic.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const Tic = () => {
-    let arr=[
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ]
+  let arr = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
 
-    const [board,setBoard]=React.useState(arr)
-    const [turn,setTurn]=React.useState(false)
-    const [winner,setWinner]=React.useState(0)
-
-    let move=(turn,row,col)=>{
-if (turn){
-arr[row][col]=1
-}else{
-    arr[row][col]=2
-}
+  const [board, setBoard] = React.useState(arr);
+  const [turn, setTurn] = React.useState(false);
+    const [winner, setWinner] = React.useState(null);
+  let move = (turn, row, col) => {
+    if (turn) {
+      let newBoard = [...board];
+      newBoard[row][col] = 1;
+      setBoard(newBoard);
+    } else {
+      let newBoard = [...board];
+      newBoard[row][col] = 2;
+      setBoard(newBoard);
     }
+  };
+  let win=(arr)=>{
+    
+    
+  }
+  
   return (
-    <div>{board.map((e,row)=>{e.map((e,col)=>{<div>{board[row][col]}</div>})})}</div>
-  )
-}
+    <div>
+      {board.map((e, r) => (
+        <div  key={r}> 
+          {e.map((e, c) => (
+            <span key={c}
+              onClick={(e) => {
+                move(turn, r, c)
+                  // console.log(e)
+                  console.log(board[r][c])
+                  if(board[r][c] == 0){
+                    setTurn(!turn);
+                  }
+                setWinner(win(board));
+            
+              }}
+            >
+              {" "}
+              {board[r][c]}{" "}
+            </span>
+          ))}
+        </div>
+      ))}
+  {winner?<h1>{winner}</h1>:null}
+    </div>
+  );
+};
 
-export default Tic
+export default Tic;
