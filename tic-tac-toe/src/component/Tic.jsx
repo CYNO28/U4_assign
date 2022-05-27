d12ff89f6bb8b14394d78622efd263d68828c815
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-05-27 21:19:21 +0530

    1

--- tic-tac-toe/src/component/Tic.jsx
+++ tic-tac-toe/src/component/Tic.jsx
@@ -1,55 +1,87 @@
 import React from "react";
-
+import style from "./Tic.module.css";
 const Tic = () => {
-  let arr = [
+  let borad = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
-
-  const [board, setBoard] = React.useState(arr);
+  const [board, setBoard] = React.useState(borad);
   const [turn, setTurn] = React.useState(false);
-    const [winner, setWinner] = React.useState(null);
-  let move = (turn, row, col) => {
-    if (turn) {
+  const [x, setX] = React.useState(null);
+  let onClickHandler = (row, col) => {
+    // console.log(board)0
+    console.log("rc", board);
+    if (board[row][col] === 0) {
       let newBoard = [...board];
-      newBoard[row][col] = 1;
+      newBoard[row][col] = turn;
       setBoard(newBoard);
-    } else {
-      let newBoard = [...board];
-      newBoard[row][col] = 2;
-      setBoard(newBoard);
+      setTurn(!turn);
     }
   };
-  let win=(arr)=>{
-    
-    
-  }
-  
+  let winner = (board) => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] === board[i][1] &&
+        board[i][1] === board[i][2] &&
+        board[i][0] !== 0
+      ) {
+        return board[i][0];
+      }
+      for (let i = 0; i < 3; i++) {
+        if (
+          board[0][i] === board[1][i] &&
+          board[1][i] === board[2][i] &&
+          board[0][i] !== 0
+        ) {
+          return board[0][i];
+        }
+      }
+      if (
+        board[0][0] === board[1][1] &&
+        board[1][1] === board[2][2] &&
+        board[0][0] !== 0
+      ) {
+        return board[0][0];
+      }
+    }
+  };
+
   return (
     <div>
-      {board.map((e, r) => (
-        <div  key={r}> 
-          {e.map((e, c) => (
-            <span key={c}
-              onClick={(e) => {
-                move(turn, r, c)
-                  // console.log(e)
-                  console.log(board[r][c])
-                  if(board[r][c] == 0){
-                    setTurn(!turn);
-                  }
-                setWinner(win(board));
-            
-              }}
-            >
-              {" "}
-              {board[r][c]}{" "}
-            </span>
-          ))}
-        </div>
-      ))}
-  {winner?<h1>{winner}</h1>:null}
+      <h1>{x === true ? "x win" : x === false ? "o win" : ""}</h1>
+      {board.map((row, rowIndex) => {
+        return (
+          <div key={rowIndex}>
+            {row.map((col, colIndex) => {
+              return (
+                <div
+                  className={style.inbox}
+                  onClick={() => {
+                    onClickHandler(rowIndex, colIndex);
+                    setX(winner(board));
+                  }}
+                  key={colIndex}
+                >
+                  {board[rowIndex][colIndex] === true
+                    ? "x"
+                    : board[rowIndex][colIndex] === false
+                    ? "o"
+                    : ""}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+
+      <button
+        onClick={() => {
+          setBoard(borad);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
