a0c6d06192b5cb60f4f1d4f29173a521c6268c32
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-05-28 14:28:49 +0530

    1

--- event/src/App.js
+++ event/src/App.js
@@ -4,28 +4,11 @@ import React, { useState } from "react";
 import Timer from "./components/Timer";
 
 function App() {
-  const [stime, setStime] = useState(0);
-  const [etime, setEtime] = useState(0);
-  return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Enter stating time"
-        onChange={(e) => {
-          setStime(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Enter end time"
-        onChange={(e) => {
-          setEtime(e.target.value);
-          console.log(etime)
-        }}
-      />
-      <Timer stime={stime} etime={etime} />
-    </div>
-  );
+return(
+  <div>
+    <Timer />
+  </div>
+)
 }
 
 export default App;
--- event/src/components/Timer.jsx
+++ event/src/components/Timer.jsx
@@ -1,31 +1,8 @@
-import React, { useEffect,useRef } from 'react'
-import {useState} from 'react'
-const Timer = (prop) => {
-  const[timer,settimer]=useState(0);
-  const timerID=useRef(null);
-  
-  
-  // useEffect(()=>{
-     function start(){ 
-      let id = setInterval(()=>{
-        settimer((timer)=>timer+1);
-        console.log( timerID.current,'id')
-    },1000)
-    timerID.current=(id);
-    if(timer >= 20){
-        // console.log(prop.etime)
-        console.log("this should work")
-        clearInterval(timerID.current);
-      }}
+import React from 'react'
 
-      // return ()=>{
-      //     clearInterval(id);
-      // }
-  // },[timer])
-    return (
-    <div>Timer:{timer}
-    <button onClick={start}>Start</button>
-    </div>
+const Timer = () => {
+  return (
+    <div>Timer</div>
   )
 }
 
