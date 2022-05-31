319813868b2d00acc8c22d0780c0e64b74a52255
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-05-31 22:00:48 +0530

    1

--- event/src/App.js
+++ event/src/App.js
@@ -4,9 +4,13 @@ import React, { useState } from "react";
 import Timer from "./components/Timer";
 
 function App() {
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(0);
 return(
-  <div>
-    <Timer />
+  <div className="App">
+    <input onChange={(e)=>setEnd(e.target.value)} placeholder="Enter End Value"/>
+    <input onChange={(e)=>setStart(e.target.value)} placeholder='Enter Start value'/>
+    <Timer start={start} setStart={setStart} end={end}/>
   </div>
 )
 }
--- event/src/components/Timer.jsx
+++ event/src/components/Timer.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const Timer = () => {
+const Timer = ({start ,setStart,end}) => {
+const[startTime,setStartTime] = React.useState(false)
+useEffect(() => {
+    console.log("unmount")
+ let interval  =null
+
+ if(startTime)starts()
+  function starts(){
+// console.log(startTime)
+    interval= setTimeout(() => {
+      setStart((count) => (Number(count) + 1));
+    }, 1000);
+  }
+  if(start>=Number(end)){
+clearInterval(interval)
+  }
+  
+    return () => {
+      clearTimeout(interval);
+    }
+   
+  },[start,startTime]);
+   
+ 
+
+  
   return (
-    <div>Timer</div>
+    <div className="App">Timer:{start}
+    <br></br>
+      <button onClick={()=>setStartTime(!startTime)} >Start</button>
+    </div>
   )
 }
 
